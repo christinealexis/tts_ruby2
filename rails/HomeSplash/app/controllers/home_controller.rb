89fe2a6fe0ee49d11aa5d7d311372f8bcd4bf07d
Christine Alexis <Your Actual Emailchristine@cultureshockstore.com>
2015-03-02 20:35:44 -0600

    got the clock and greeting working

--- rails/HomeSplash/app/controllers/home_controller.rb
+++ rails/HomeSplash/app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
+
+  	@current_time = Time.now.to_formatted_s(:short)
+  	@greet_message = case Time.now.hour
+  	when 5..11	
+  		"Good mernin"
+  	when 12..15
+  		"Good Afternoon, SUCKA"
+  	when 16..19
+  		["Buenas Afternoonas", "afternoon!"].sample
+  	when 20..23, 0..4
+  		"Gnight!"
+  	end
+
   end
 end
